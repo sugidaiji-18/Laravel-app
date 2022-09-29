aaeed525bea5c4b755dd731a66152b1ee2efc3b1
sugidaiji-18 <sugidaiji@gmail.com>
2022-09-30 00:54:39 +0900

    各種設定
    時刻変更、adminlteなど

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('adminlte',function(){
+    return view('adminlte');
+});
+Route::get('/hello',function(){
+    return view('hello');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
